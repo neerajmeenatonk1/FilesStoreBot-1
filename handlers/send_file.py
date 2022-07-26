e08cf10823aba2d62a43deb9ad597b7fac9b2e25
Gaurav4x <102266943+Gauravbots@users.noreply.github.com>
2022-07-27 00:08:52 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,12 +11,10 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"**Here is Sharable Link of this file:**\n"
-            f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{str_to_b64(str(file_id))}\n\n"
-            f"__To Retrive the Stored File, just open the link!__",
+            f"**🥳 Your file is ready to use!**\n",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
-        await asyncio.sleep(e.value)
+        await asyncio.sleep(e.x)
         await reply_forward(message, file_id)
 
 
@@ -29,7 +27,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
-        await asyncio.sleep(e.value)
+        await asyncio.sleep(e.x)
         return media_forward(bot, user_id, file_id)
 
 
